1f9a735a813f1852aa34ffb5b23bd3c2886db559
cosmicthemethhead <thomasbohrer5@gmail.com>
2023-02-04 22:50:56 +0100

    feat(glfw): addded init, window interation and termination

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
-#include <iostream>
+#include <cstdio>
+#include <GLFW/glfw3.h>
 
 int main() {
-  std::cout << "Programed to work and not to feel!" << std::endl;
+  if (!glfwInit()) {
+    printf("error:glfw - failed to initialize\n");
+    return -1;
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* wnd = glfwCreateWindow(800, 600, "snek", nullptr, nullptr);
+  if (wnd == nullptr) {
+    printf("error:glfw:window - failed to initialize\n");
+    glfwTerminate();
+    return -1;
+  }
+
+  while (!glfwWindowShouldClose(wnd)) {
+    glfwSwapBuffers(wnd);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
   return 0;
 }
