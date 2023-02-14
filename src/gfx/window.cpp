bd2ed5ebcb614ee75371714dd80cb2c407358774
cosmicthemethhead <thomasbohrer5@gmail.com>
2023-02-14 11:57:08 +0100

    feat(window): added "glfwMakeContextCurrent" in init

--- src/gfx/window.cpp
+++ src/gfx/window.cpp
@@ -6,12 +6,17 @@
 
 namespace gfx {
   void Window::init(const char title[]) {
-    this->internal_win = glfwCreateWindow(800, 600, title, nullptr, nullptr);
+    this->internal_win = glfwCreateWindow(
+        WIN_WIDTH, WIN_HEIGHT,
+        title, nullptr, nullptr);
+
     if (this->internal_win == nullptr) {
       printf("error:glfw:window - failed to initialize\n");
       glfwTerminate();
       exit(-1);
     }
+
+    glfwMakeContextCurrent(this->internal_win);
   }
 
   void Window::terminate() {
